a930bb7f0ed10bdcd6a675aeaa79dfff2cc75b84
Praxis <hello@vulpinecat.com>
2021-07-16 11:19:00 +0200

    add prettier

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
-import { initalize, loadById } from "./modules/schema";
+import { initalize, loadById } from './modules/schema';
 
-require('dotenv').config()
+require('dotenv').config();
 initalize();
-const server = loadById("1");
+const server = loadById('1');
 if (server.server.state === undefined) {
-    // Contact Hetzner API here
+  // Contact Hetzner API here
 
-    console.log(server)
-
+  console.log(server);
 }
 
-console.log("loaded without errors");
+console.log('loaded without errors');
